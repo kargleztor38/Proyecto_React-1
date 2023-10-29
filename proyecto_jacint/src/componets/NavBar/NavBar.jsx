f4741cb630cc567203375e06a3937ea59a294ea5
karlay <karglrztor84@gmail.com>
2023-10-29 21:49:25 +0100

    re-entregando

--- proyecto_jacint/src/componets/NavBar/NavBar.jsx
+++ proyecto_jacint/src/componets/NavBar/NavBar.jsx
@@ -5,20 +5,12 @@ export const NavBar = () => {
   return (
     <nav className="navBar">
       <h2 className="navLogo">mi tienda</h2>
-      <ul className="navMenu">
-        <li>
-          <a href="#">inicio</a>
-        </li>
-        <li>
-          <a href="#">producto del autor</a>
-        </li>
-        <li>
-          <a href="#">productos mas populares</a>
-        </li>
-        <li>
-          <a href="#">otros productos</a>
-        </li>
-      </ul>
+
+      <button>inicio</button>
+      <button>producto del autor</button>
+      <button>productos mas populares</button>
+      <button>otros productos</button>
+
       <CartWidget />
     </nav>
   );
